c8b4138cdce2c5b26d376a67460983167f21a945
polatemre <41800395+polatemre@users.noreply.github.com>
2021-05-21 23:00:12 +0300

    init project

---
+++ ChatAppServer/Database/ClientSource.cs
@@ -0,0 +1,13 @@
+﻿using ChatAppServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAppServer.Database
+{
+    public static class ClientSource
+    {
+        public static List<Client> Clients { get; } = new List<Client>();
+    }
+}
---
+++ ChatAppServer/Database/GroupSource.cs
@@ -0,0 +1,13 @@
+﻿using ChatAppServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAppServer.Database
+{
+    public static class GroupSource
+    {
+        public static List<Group> Groups { get; } = new List<Group>();
+    }
+}
